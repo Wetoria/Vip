08b1cc10f673296254cc7f5e13ddbe5132e6b35b
Wetoria <>
2022-09-18 19:16:09 +0800

    modify my web site base structure

---
+++ dev/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  
+</template>
+
+
+
+<script setup>
+import { onMounted } from '@vue/runtime-core'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+onMounted(() => {
+  console.log('test');
+  window.open('./blog/index.html', '_self');
+}) 
+
+</script>
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
