d56c7f241fb2a06205939b8336324104921af54a
Wetoria <>
2022-09-24 22:49:08 +0800

    update home page

--- dev/src/Index.vue
+++ dev/src/Index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container wh100">
     <span>
-      <span>最新的博客内容，请查看语雀：<a href="https://www.yuque.com/wetoria">Wetoria</a></span><br />
+      <SimpleFrontend></SimpleFrontend>
+      <span>关于我：<a href="https://www.yuque.com/wetoria">Wetoria</a></span><br />
+      <span>最新文章：<a href="https://www.yuque.com/wetoria/essays">I'm Vip</a></span><br />
       <span>练习项目地址：<router-link to="/simple-frontend/practices/index">Practices</router-link></span>
     </span>
   </div>
 </template>
 
-<script>
+<script setup>
+import SimpleFrontend from './simple-frontend'
 </script>
 
 <style scoped>
