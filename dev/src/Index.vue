fc18452e8000c57e2e36eeda228394b18f3417a1
Wetoria <>
2022-11-05 18:11:07 +0800

    Add SF home link

--- dev/src/Index.vue
+++ dev/src/Index.vue
@@ -4,7 +4,8 @@
       <SimpleFrontend></SimpleFrontend>
       <span>关于我：<a href="https://www.yuque.com/wetoria">Wetoria</a></span><br />
       <span>最新文章：<a href="https://www.yuque.com/wetoria/essays">I'm Vip</a></span><br />
-      <span>练习项目地址：<router-link to="/simple-frontend/practices/index">Practices</router-link></span>
+      <span>前端学习平台：<a href="https://www.yuque.com/wetoria/simplest-frontend">极简化前端</a></span><br />
+      <span>练习项目地址：<router-link to="/simple-frontend/practices/index">Practices</router-link></span><br />
     </span>
   </div>
 </template>
