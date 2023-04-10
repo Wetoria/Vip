5b608d0cf14840f73228215333a1af0ce767d69f
Vip <vwetoria@gmail.com>
2023-04-10 08:34:37 +0800

    publish

--- dev/src/Tools/ChangeVersion.mjs
+++ dev/src/Tools/ChangeVersion.mjs
@@ -1,19 +1,18 @@
 import SystemInfo from '../../public/data/SystemInfo.mjs';
-console.log('System info is ', SystemInfo);
+// console.log('System info is ', SystemInfo);
 const lastVersion = SystemInfo.version
 const versions = lastVersion.split('.')
 const lastThirdVersion = versions[2]
 const newThirdVersion = Number(lastThirdVersion) + 1
-console.log(newThirdVersion);
+// console.log(newThirdVersion);
 const newVersion = [...versions]
 newVersion[2] = newThirdVersion
 SystemInfo.version = newVersion.join('.')
-console.log('New System info is ', JSON.stringify(SystemInfo, null, 2));
+// console.log('New System info is ', JSON.stringify(SystemInfo, null, 2));
 
 import fs from 'fs'
-try {
-  fs.writeFileSync('../../public/data/SystemInfo.mjs', JSON.stringify(SystemInfo, null, 2))
-} catch (err) {
-  console.error(err)
-}
+fs.writeFileSync('./public/data/SystemInfo.mjs', `export default ${JSON.stringify(SystemInfo, null, 2)}`)
+// fs.readdir('./', (...args) => {
+//   console.log('list is ', ...args);
+// })
 
