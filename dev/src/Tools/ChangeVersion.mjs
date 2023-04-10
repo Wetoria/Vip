f64e0c93d48d89318a79599d978b7645b9c07dff
Vip <vwetoria@gmail.com>
2023-04-10 08:24:31 +0800

    publish

--- dev/src/Tools/ChangeVersion.mjs
+++ dev/src/Tools/ChangeVersion.mjs
@@ -5,3 +5,10 @@ const versions = lastVersion.split('.')
 const lastThirdVersion = versions[2]
 const newThirdVersion = Number(lastThirdVersion) + 1
 console.log(newThirdVersion);
+const newVersion = [...versions]
+newVersion[2] = newThirdVersion
+SystemInfo.version = newVersion.join('.')
+console.log('New System info is ', JSON.stringify(SystemInfo, null, 2));
+
+// import fs from 'fs'
+
