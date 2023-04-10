c0cc2c71687cc4fa7b0362317c7c684e32daaa82
Vip <vwetoria@gmail.com>
2023-04-10 09:32:48 +0800

    feat: 调整修改版本的相关逻辑

--- dev/src/Tools/ChangeVersion.js
+++ dev/src/Tools/ChangeVersion.js
@@ -1,4 +1,4 @@
-import SystemInfo from '../../public/data/SystemInfo.json';
+const SystemInfo = require('../../public/data/SystemInfo.json');
 // console.log('System info is ', SystemInfo);
 const lastVersion = SystemInfo.version
 const versions = lastVersion.split('.')
@@ -10,7 +10,7 @@ newVersion[2] = newThirdVersion
 SystemInfo.version = newVersion.join('.')
 // console.log('New System info is ', JSON.stringify(SystemInfo, null, 2));
 
-import fs from 'fs'
+const fs = require('fs')
 fs.writeFileSync('./public/data/SystemInfo.json', `${JSON.stringify(SystemInfo, null, 2)}`)
 // fs.readdir('./', (...args) => {
 //   console.log('list is ', ...args);
